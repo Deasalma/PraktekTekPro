e8bbc019a29c7d29475c265d3e5dffad3873d251
DeaSalma <92972256+Deasalma@users.noreply.github.com>
2022-03-20 16:32:09 +0700

    Add files via upload

--- Minggu6/Aggretation/main.java
+++ Minggu6/Aggretation/main.java
@@ -1,1 +1,18 @@
+package Aggregation;
 
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		School sekolah1 = new School("SMAN 1 Bandung", "Bandung");
+		School sekolah2 = new School("SMAN 2 Bandung", "Bandung");
+		
+		Siswa s1 = new Siswa("Budi", sekolah1);
+		Siswa s2 = new Siswa("Citra", sekolah2);
+		
+		s1.display();
+		System.out.println("\n");
+		s2.display();
+	}
+
+}
