a4eed28c10fd563677e58f2ca9653df1848107b3
DeaSalma <92972256+Deasalma@users.noreply.github.com>
2022-03-20 16:37:44 +0700

    Add files via upload

---
+++ Minggu6/School.java
@@ -0,0 +1,13 @@
+package Aggregation;
+
+public class School {
+		String nama;
+		String address;
+		
+public School(String nama, String address) {  
+		this.nama = nama;
+		this.address = address;
+}  
+
+}
+
