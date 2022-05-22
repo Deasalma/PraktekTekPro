f3a09a915ed0ca37f1461985ad7c28d7bb623947
DeaSalma <92972256+Deasalma@users.noreply.github.com>
2022-05-22 19:55:07 +0700

    Add files via upload

---
+++ Minggu 13/monopoly/GoCell.java
@@ -0,0 +1,16 @@
+package com.polban.tekpro.monopoly;
+
+public class GoCell extends Cell {
+	protected Player owner;
+
+	public GoCell() {
+		super.setName("Go");
+
+	}
+
+	public void playAction() {
+	}
+	
+	void setName(String name) {
+	}
+}
